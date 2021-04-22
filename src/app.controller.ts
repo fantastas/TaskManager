8204f0e45a0586a6d7e5556831299fa15c02831e
fantastas <msgitara@gmail.com>
2021-04-22 11:06:34 +0300

    a

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,25 +1,9 @@
-import { AuthService } from './auth/auth.service';
-import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
-// import { AuthGuard } from '@nestjs/passport';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { LocalAuthGuard } from './auth/local-auth.guard';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService, // private readonly authService: AuthService,
   ) {}
-
-  // @UseGuards(LocalAuthGuard)
-  // @Post('auth/login')
-  // async login(@Req() req) {
-  //   return this.authService.login(req.user);
-  // }
-
-  // @UseGuards(JwtAuthGuard)
-  // @Get('profile')
-  // getProfile(@Req() req) {
-  //   return req.user;
-  // }
 }
